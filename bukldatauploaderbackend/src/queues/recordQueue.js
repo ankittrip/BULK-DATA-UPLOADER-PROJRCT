88317bcbb402a83e9fe5f6e6743930f9929780f5
Ankit Tripathi <168020066+ankittrip@users.noreply.github.com>
2025-06-21 16:59:33 +0530

    Update recordQueue.js

--- bukldatauploaderbackend/src/queues/recordQueue.js
+++ bukldatauploaderbackend/src/queues/recordQueue.js
@@ -1,10 +1,15 @@
-import { Queue } from "bullmq"
-import dotenv from "dotenv"
-dotenv.config()
+import { Queue } from "bullmq";
+import dotenv from "dotenv";
+dotenv.config();
 
-const connection = {
-  host: "127.0.0.1",
-  port: 6379,
+const redisUrl = process.env.REDIS_URL;
+
+if (!redisUrl) {
+  throw new Error("REDIS_URL is not defined in the environment variables");
 }
 
-export const recordQueue = new Queue("record-queue", { connection })
+export const recordQueue = new Queue("record-queue", {
+  connection: {
+    url: redisUrl,
+  },
+});
