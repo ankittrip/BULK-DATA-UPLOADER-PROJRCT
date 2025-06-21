4b8a3b1d36fc3352d9cf816bdea8fde49af0cb72
Ankit Tripathi <168020066+ankittrip@users.noreply.github.com>
2025-06-21 16:58:12 +0530

    Update redis.js

--- bukldatauploaderbackend/src/config/redis.js
+++ bukldatauploaderbackend/src/config/redis.js
@@ -1,45 +1,30 @@
-// config/redisClient.js
-import { createClient } from "redis";
+
 import dotenv from "dotenv";
 dotenv.config();
 
-const isRedisCloud = process.env.REDIS_URL?.startsWith("rediss://");
+const redisUrl = process.env.REDIS_URL;
 
+if (!redisUrl) throw new Error("REDIS_URL not defined");
 
-const redisClient = createClient({
-  url: process.env.REDIS_URL,
-  socket: {
-    tls: isRedisCloud,
-  },
+const redisClient = new Redis({
+  host: "redis-17191.c322.us-east-1-2.ec2.redns.redis-cloud.com",
+  port: 17191,
+  password: process.env.REDIS_PASSWORD,
 });
 
-redisClient.on("error", (err) => {
-  console.error("Redis Client Error:", err);
-});
+redisClient.on("connect", () => console.log("Connecting to Redis..."));
+redisClient.on("ready", () => console.log("Redis client connected and ready."));
+redisClient.on("error", (err) => console.error("Redis Client Error:", err));
+redisClient.on("end", () => console.log("Redis connection closed."));
 
-redisClient.on("connect", () => {
-  console.log("Connecting to Redis...");
-});
-
-redisClient.on("ready", () => {
-  console.log("Redis client connected and ready.");
-});
-
-redisClient.on("end", () => {
-  console.log("Redis connection closed.");
-});
-
-// Connect Redis
-const connectRedis = async () => {
+export const connectRedis = async () => {
   try {
-    if (!redisClient.isOpen) {
-      await redisClient.connect();
-    }
+    await redisClient.ping();
+    console.log("Redis PING successful.");
   } catch (err) {
     console.error("Redis connection failed:", err);
     throw err;
   }
 };
 
-export { connectRedis };
 export default redisClient;
