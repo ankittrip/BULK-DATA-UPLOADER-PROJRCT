1b86da33954e34ae18703cfdfe0e4d009c9e2f84
Ankit Tripathi <168020066+ankittrip@users.noreply.github.com>
2025-06-25 06:33:09 +0530

    Update redis.js

--- bukldatauploaderbackend/src/config/redis.js
+++ bukldatauploaderbackend/src/config/redis.js
@@ -13,6 +13,7 @@ const redisClient = createClient({
 
     // TLS must be false or removed
     // tls: false ← OPTIONAL; default is false
+    tls: process.env.REDIS_TLS === 'true',  // Optional logic
 
     reconnectStrategy: (retries) => Math.min(retries * 100, 5000),
   },
